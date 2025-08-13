d5fdadf0d828f330489fd48bbfb077cdaadc0f67
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-08-13 09:05:21 -0300

    Update - Script do banco de dados digitado

---
+++ banco_dados/banco.sql
@@ -0,0 +1,59 @@
+-- Criar o banco de dados
+
+CREATE DATABASE padaria;
+
+
+-- Usar o banco de dados
+
+USE padaria;
+
+-- Criar a tabela de usuários (funcionário da padaria)
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Criar tabela de clientes 
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Criar tabela de produtos da padaria
+
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    quantidade_estoque INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Insert do usuário
+
+INSERT INTO 'usuarios'('id', 'nome', 'email') VALUES ('NULL', 'João', 'joao@gmail.com');
+
+-- Insert do cliente
+
+INSERT INTO 'clientes'('id', 'nome', 'email') VALUES ('NULL', 'Maria', 'maria@gmail.com');
+
+-- Insert dos produtos
+
+INSERT INTO `produtos`(`id`, `nome`, `quantidade_estoque`)
+VALUES 
+('NULL','Pão','93')
+('NULL','Pão de queijo','34')
+('NULL','Pastel','17')
+('NULL','Cuca','12')
+('NULL','Bolo','20')
+('NULL','Sonho','45')
+('NULL','Empada','29')
+('NULL','Café','83')
+('NULL','Rosca','19')
+('NULL','Bolinho de Carne','24')
+ 
