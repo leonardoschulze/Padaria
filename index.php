845c1b2ec13ee3007fbce2cd2f3200827e7b7a3b
juan-sanz22 <juan_sanz@estudante.sesisenai.org.br>
2025-08-13 09:42:57 -0300

    UPDATE - fiz o primeiro esqueletp do site

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Padaria Pão e Confia</title>
+</head>
+<body>
+<?php
+  echo "<h1>Bem-vindo à Pão e Confia!</h1>";
+  echo "<p>Padaria Feita para te fazer feliz, faça a sua compra</p>";
+?>
+  <section>
+    <div class="row-produtos">
+        <div class="produtos">
+          <h1>Pão</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Pão Queijo</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Pastel</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Cuca</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Bolo</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Sonho</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Empada</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Café</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Rosca</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+        <div class="produtos">
+          <h1>Bolinho de Carne</h1>
+          <button class="menu-btn">
+                <a href=""><i class=""></i></a>
+            </button>
+        </div>
+    </div>
+  </section>
+</body>
+</html>
